c0f0f232adad715791cde3622939b463d925c165
Tom Schrier <54577949+TomSchrier@users.noreply.github.com>
2026-02-05 17:26:15 +0100

    feat(locale): add Norwegian (nb_NO) continent definitions (#3712)

--- src/locales/nb_NO/location/index.ts
+++ src/locales/nb_NO/location/index.ts
@@ -8,6 +8,7 @@ import city_name from './city_name';
 import city_pattern from './city_pattern';
 import city_suffix from './city_suffix';
 import common_street_suffix from './common_street_suffix';
+import continent from './continent';
 import county from './county';
 import direction from './direction';
 import postcode from './postcode';
@@ -25,6 +26,7 @@ const location: LocationDefinition = {
   city_pattern,
   city_suffix,
   common_street_suffix,
+  continent,
   county,
   direction,
   postcode,
